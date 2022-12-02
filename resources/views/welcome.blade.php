5a37d44ab3fad6ab78b29b75a5614c7bbb3178fa
leed lust <khalidkoukou70@gmail.com>
2022-12-02 17:29:48 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+@section('content')
+        <div class = 'header1'>
+        </div>
+        <img class='welcome' src="storage/image/welcome.png" width='200' height='200'/>
+       
+@endsection  
